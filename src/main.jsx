f2945fad7e818f8b1aebfbd34a83a51109e5cb36
alexis <alexisamadei@gmail.com>
2025-12-03 22:52:44 +0100

    clean after major upgrade

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,8 @@ import './index.css'
 import { BrowserRouter } from 'react-router'
 import { Provider } from './components/ui/provider.jsx'
 import App from './views/App.jsx'
+import { createRoot } from 'react-dom/client'
+import { StrictMode } from 'react'
 
 createRoot(document.getElementById('root')).render(
     <StrictMode>
